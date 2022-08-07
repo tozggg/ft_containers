b3e6de26d028c3f139fb38e680da1992923ae198
tozggg <tozggg@gmail.com>
2022-08-07 17:10:06 +0900

    iterator, utils

---
+++ includes/iterator.hpp
@@ -0,0 +1,193 @@
+#ifndef ITERATOR_HPP
+# define ITERATOR_HPP
+
+# include <cstddef>
+
+namespace ft
+{
+	/* iterator_traits */
+	template <class Iterator>
+	struct iterator_traits
+	{
+		typedef typename Iterator::difference_type  	difference_type;
+		typedef typename Iterator::value_type       	value_type;
+		typedef typename Iterator::pointer          	pointer;
+		typedef typename Iterator::reference        	reference;
+		typedef typename Iterator::iterator_category	iterator_category;
+	};
+
+	template <typename T>
+	struct iterator_traits<T*>
+	{
+		typedef std::ptrdiff_t                  	difference_type;
+		typedef T                               	value_type;
+		typedef T*                              	pointer;
+		typedef T&                              	reference;
+		typedef std::random_access_iterator_tag 	iterator_category;
+	};
+
+	template <typename T>
+	struct iterator_traits<const T*>
+	{
+		typedef std::ptrdiff_t                  	difference_type;
+		typedef T                               	value_type;
+		typedef const T*                        	pointer;
+		typedef const T&                        	reference;
+		typedef std::random_access_iterator_tag 	iterator_category;
+	};
+
+
+	/* reverse_iterator */
+	template <class Iterator>
+	class reverse_iterator {
+		public:
+			typedef Iterator                                                iterator_type;
+    		typedef typename iterator_traits<Iterator>::iterator_category   iterator_category;
+    		typedef typename iterator_traits<Iterator>::value_type          value_type;
+    		typedef typename iterator_traits<Iterator>::difference_type     difference_type;
+    		typedef typename iterator_traits<Iterator>::pointer             pointer;
+    		typedef typename iterator_traits<Iterator>::reference           reference;
+		
+		private:
+			iterator_type _base;
+		
+		public:
+			reverse_iterator() : _base() {}
+			explicit reverse_iterator(iterator_type it) : _base(it) {}
+			template <class Iter>
+    		reverse_iterator(const reverse_iterator<Iter>& rit) : _base(rit.base()) {}
+
+			reverse_iterator operator=(const reverse_iterator& rhs)
+			{
+				_base_iterator = rhs._base_iterator;
+				return *this;
+			}
+
+			iterator_type base() const { return _base; }
+
+			reference operator*() const { return *(_base - 1); }
+			pointer operator->() const { return &(operator*()); }
+			reference operator[](difference_type n) const { return *(_base - 1 - n) }
+			reverse_iterator& operator++() { --_base; return *this; }
+			reverse_iterator operator++(int) { reverse_iterator tmp(*this); --_base; return tmp; }
+			reverse_iterator& operator--() { ++_base; return *this; }
+			reverse_iterator operator--(int) { reverse_iterator tmp(*this); ++_base; return tmp; }
+			reverse_iterator operator+(difference_type n) const { return reverse_iterator(_base - n); }
+			reverse_iterator& operator+=(difference_type n) { _base -= n; return *this; }
+			reverse_iterator operator-(difference_type n) const { return reverse_iterator(_base + n); }
+			reverse_iterator& operator-=(difference_type n) { _base += n; return *this; }
+	};
+
+	template <class IterL, class IterR>
+	bool operator==(const reverse_iterator<IterL>& lhs, const reverse_iterator<IterR>& rhs)
+	{ return lhs.base() == rhs.base(); }
+
+	template <class IterL, class IterR>
+	bool operator!=(const reverse_iterator<IterL>& lhs, const reverse_iterator<IterR>& rhs)
+	{ return lhs.base() != rhs.base(); }
+
+	template <class IterL, class IterR>
+	bool operator<(const reverse_iterator<IterL>& lhs, const reverse_iterator<IterR>& rhs)
+	{ return lhs.base() > rhs.base(); }
+
+	template <class IterL, class IterR>
+	bool operator<=(const reverse_iterator<IterL>& lhs, const reverse_iterator<IterR>& rhs)
+	{ return lhs.base() >= rhs.base(); }
+
+	template <class IterL, class IterR>
+	bool operator>(const reverse_iterator<IterL>& lhs, const reverse_iterator<IterR>& rhs)
+	{ return lhs.base() < rhs.base(); }
+
+	template <class IterL, class IterR>
+	bool operator>=(const reverse_iterator<IterL>& lhs, const reverse_iterator<IterR>& rhs)
+	{ return lhs.base() <= rhs.base(); }
+
+	template <class Iter>
+	reverse_iterator<Iter> operator+(typename reverse_iterator<Iter>::difference_type n, const reverse_iterator<Iter>& rhs)
+	{ return rhs + n; }
+
+	template <class IterL, class IterR>
+	typename reverse_iterator<IterL>::difference_type operator-(const reverse_iterator<IterL>& lhs, const reverse_iterator<IterR>& rhs)
+	{ return rhs.base() - lhs.base(); }
+
+
+	/* vector_iterator */
+	// =random access iterator
+	template <typename T>
+	class vector_iterator {
+		public:
+			typedef T								value_type;
+			typedef T*								pointer;
+			typedef T&								reference;
+			typedef ptrdiff_t						difference_type;
+			typedef std::random_access_iterator_tag	iterator_category;
+
+		private:
+			pointer _ptr;
+
+		public:
+			vector_iterator() : _ptr(NULL) {}
+			vector_iterator(pointer p) : _ptr(p) {}
+			~vector_iterator() {}
+			vector_iterator(const vector_iterator &other) : _ptr(other._ptr) {}
+
+			vector_iterator &operator=(const vector_iterator &rhs)
+			{
+				_ptr = rhs._ptr;
+				return *this;
+			}
+
+			pointer base(void) const { return _ptr; }
+
+			reference operator*() const { return *_ptr; }
+			pointer operator->() const { return &(operator*()); }
+			reference operator[](difference_type n) const { return _ptr[n]; }
+			vector_iterator& operator++() { ++_ptr; return *this; }
+			vector_iterator operator++(int) { vector_iterator tmp(*this); ++_ptr; return tmp; }
+			vector_iterator& operator--() { --_ptr; return *this; }
+			vector_iterator operator--(int) { vector_iterator tmp(*this); --_ptr; return tmp; }
+			vector_iterator operator+(difference_type n) const { return _ptr + n; }
+			vector_iterator& operator+=(difference_type n) { _ptr += n; return *this; }
+			vector_iterator operator-(difference_type n) const { return _ptr - n }
+			vector_iterator& operator-=(difference_type n) { _ptr -= n; return *this; }
+	};
+
+	template <class IterL, class IterR>
+	bool operator==(const vector_iterator<IterL>& lhs, const vector_iterator<IterR>& rhs)
+	{ return lhs.base() == rhs.base(); }
+
+	template <class IterL, class IterR>
+	bool operator!=(const vector_iterator<IterL>& lhs, const vector_iterator<IterR>& rhs)
+	{ return lhs.base() != rhs.base(); }
+
+	template <class IterL, class IterR>
+	bool operator<(const vector_iterator<IterL>& lhs, const vector_iterator<IterR>& rhs)
+	{ return lhs.base() > rhs.base(); }
+
+	template <class IterL, class IterR>
+	bool operator<=(const vector_iterator<IterL>& lhs, const vector_iterator<IterR>& rhs)
+	{ return lhs.base() >= rhs.base(); }
+
+	template <class IterL, class IterR>
+	bool operator>(const vector_iterator<IterL>& lhs, const vector_iterator<IterR>& rhs)
+	{ return lhs.base() < rhs.base(); }
+
+	template <class IterL, class IterR>
+	bool operator>=(const vector_iterator<IterL>& lhs, const vector_iterator<IterR>& rhs)
+	{ return lhs.base() <= rhs.base(); }
+
+	template <class Iter>
+	vector_iterator<Iter> operator+(typename vector_iterator<Iter>::difference_type n, const vector_iterator<Iter>& rhs)
+	{ return rhs + n; }
+
+	template <class IterL, class IterR>
+	typename vector_iterator<IterL>::difference_type operator-(const vector_iterator<IterL>& lhs, const vector_iterator<IterR>& rhs)
+	{ return rhs.base() - lhs.base(); }
+
+
+	/* tree_iterator */
+	// =bidirectional iterator
+
+}
+
+#endif
---
+++ includes/utils.hpp
@@ -0,0 +1,158 @@
+#ifndef UTILS_HPP
+# define UTILS_HPP
+
+namespace ft
+{
+	/* enable_if */
+	template <bool Cond, typename T = void>
+	struct enable_if { };
+
+	template <typename T>
+	struct enable_if<true, T> { typedef T type; };
+
+
+	/* is_integral */
+	struct true_type
+	{
+		static const bool value = true;
+		operator bool() const { return value;}
+	};
+	struct false_type
+	{
+		static const bool value = false;
+		operator bool() { return value;}
+	};
+
+	template<typename T>
+	struct is_integral : public false_type {};
+
+	template<>	struct is_integral<bool> : public true_type {};
+	template<>	struct is_integral<char> : public true_type {};
+	template<>	struct is_integral<char16_t> : public true_type {};
+	template<>	struct is_integral<char32_t> : public true_type {};
+	template<>	struct is_integral<wchar_t> : public true_type {};
+	template<>	struct is_integral<signed char> : public true_type {};
+	template<>	struct is_integral<short> : public true_type {};
+	template<>	struct is_integral<int> : public true_type {};
+	template<>	struct is_integral<long> : public true_type {};
+	template<>	struct is_integral<long long> : public true_type {};
+	template<>	struct is_integral<unsigned char> : public true_type {};
+	template<>	struct is_integral<unsigned short> : public true_type {};
+	template<>	struct is_integral<unsigned int> : public true_type {};
+	template<>	struct is_integral<unsigned long> : public true_type {};
+	template<>	struct is_integral<unsigned long long> : public true_type {};
+
+
+	/* equal */
+	template <typename InputIterator1, typename InputIterator2>
+	bool equal (InputIterator1 first1, InputIterator1 last1, InputIterator2 first2)
+	{
+		while (first1 != last1)
+		{
+			if (!(*first1 == *first2))
+				return false;
+			++first1; ++first2;
+		}
+		return true;
+	}
+
+	template <typename InputIterator1, typename InputIterator2, typename BinaryPredicate>
+	bool equal (InputIterator1 first1, InputIterator1 last1, InputIterator2 first2, BinaryPredicate pred)
+	{
+		while (first1 != last1) {
+			if (!pred(*first1, *first2))
+				return false;
+			++first1; ++first2;
+		}
+		return true;
+	}
+
+
+	/* lexicographical_compare */
+	template <typename InputIterator1, typename InputIterator2>
+	bool lexicographical_compare (InputIterator1 first1, InputIterator1 last1,\
+								InputIterator2 first2, InputIterator2 last2)
+	{
+		while (first1 != last1)
+		{
+			if (first2 == last2 || *first2 < *first1)
+				return false;
+			else if (*first1 < *first2)
+				return true;
+			++first1; ++first2;
+		}
+		return (first2 != last2);
+	}
+
+	template <typename InputIterator1, typename InputIterator2, typename Compare>
+	bool lexicographical_compare (InputIterator1 first1, InputIterator1 last1,\
+								InputIterator2 first2, InputIterator2 last2, Compare comp)
+	{
+		while (first1 != last1)
+		{
+			if (first2 == last2 || comp(*first2, *first1))
+				return false;
+			else if (comp(*first1, *first2))
+				return true;
+			++first1; ++first2;
+		}
+		return (first2 != last2);
+	}
+
+
+	/* pair */
+	template <typename T1, typename T2>
+	struct pair
+	{
+		typedef T1	first_type;
+		typedef T2	second_type;
+
+		first_type	first;
+		second_type	second;
+
+		pair() : first(), second() {}
+		pair(const first_type& a, const second_type& b) : first(a), second(b) {}
+		template<typename U, typename V>
+		pair(const pair<U,V>& pr) : first(pr.first), second(pr.second) {}
+
+		pair& operator=(const pair& pr) {
+			if (this != &pr) {
+				first = pr.first;
+				second = pr.second;
+			}
+			return *this;
+		}
+	};
+
+	template <typename T1, typename T2>
+	bool operator==(const pair<T1,T2>& p1, const pair<T1,T2>& p2)
+	{ return p1.first == p2.first && p1.second == p2.second; }
+
+	template <typename T1, typename T2>
+	bool operator!=(const pair<T1,T2>& p1, const pair<T1,T2>& p2)
+	{ return !(p1 == p2); }
+
+	template <typename T1, typename T2>
+	bool operator<(const pair<T1,T2>& p1, const pair<T1,T2>& p2)
+	{ return (p1.first < p2.first) || (!(p2.first < p1.first) && p1.second < p2.second); }
+
+	template <typename T1, typename T2>
+	bool operator<=(const pair<T1,T2>& p1, const pair<T1,T2>& p2)
+	{ return !(p2 < p1); }
+
+	template <typename T1, typename T2>
+	bool operator>(const pair<T1,T2>& p1, const pair<T1,T2>& p2)
+	{ return p2 < p1; }
+
+	template <typename T1, typename T2>
+	bool operator>=(const pair<T1,T2>& p1, const pair<T1,T2>& p2)
+	{ return !(p1 < p2); }
+	
+	
+	/* make_pair */
+	template <class T1, class T2>
+	pair<T1, T2> make_pair(T1 x, T2 y)
+	{ return (pair<T1, T2>(x, y)); }
+}
+
+#endif
